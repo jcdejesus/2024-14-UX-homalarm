9e518f22bebf7fb9c2f874530536802d72d2a898
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2024-09-23 09:01:12 -0500

    Refactor GroupComponent to add create group functionality and display toast notifications

--- web/src/pages/Home/components/groups/GroupComponent.tsx
+++ web/src/pages/Home/components/groups/GroupComponent.tsx
@@ -1,33 +1,32 @@
 import React, { useState } from 'react';
 import {
   Box,
-  Card,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  IconButton,
-  Typography,
+  SnackbarCloseReason,
 } from '@mui/material';
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import EditIcon from '@mui/icons-material/Edit';
 import { GroupSidebar } from './GroupSidebar';
 import { theme } from '../../../../styles/Theme';
 import { BlueButtonText } from '../../../../components/Button/BlueButton';
 import MemberCard from './MemberCard';
+import { CustomDialog } from '../../../../components/Dialog/Dialog';
+import { Texture, ToastBar } from '../../../../components/ToastBar/ToastBar';
+import { InputText } from '../../../../components/Input/InputText';
 const initialGroups = [
   {
     id: '1',
     text: 'Grupo 1',
     members: [
-      { id: '1', name:"Juan", img: 'https://picsum.photos/id/64/250' },
-      { id: '2', name:"Pepé", img: 'https://picsum.photos/id/237/250' },
-      { id: '3', name:"Axis", img: 'https://picsum.photos/id/375/250' },
+      { id: '1', name: 'Juan', img: 'https://picsum.photos/id/64/250' },
+      { id: '2', name: 'Pepé', img: 'https://picsum.photos/id/237/250' },
+      { id: '3', name: 'Axis', img: 'https://picsum.photos/id/375/250' },
     ],
   },
   {
     id: '2',
     text: 'Grupo 2',
-    members: [{ id: '1', name:"Fer", img: 'https://picsum.photos/id/399/250' }],
+    members: [
+      { id: '1', name: 'Fer', img: 'https://picsum.photos/id/399/250' },
+    ],
   },
 ];
 const drawerWidth = 327;
@@ -35,14 +34,15 @@ const drawerWidth = 327;
 export const GroupComponent: React.FC = () => {
   const [groups, setGroups] = useState(initialGroups);
   const [selectedGroupId, setSelectedGroupId] = useState<string>('1');
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+  const [snackOpen, setSnackOpen] = React.useState(false);
+  const [snacktype, setSnackType] = React.useState<{
+    text: string;
+    texture: Texture;
+  }>({ text: 'Fallo crear grupo', texture: 'Error' });
 
   const handleCreateGroup = () => {
-    const newGroup = {
-      id: `${groups.length + 1}`,
-      text: `Grupo ${groups.length + 1}`,
-      members: [] as { id: string; name:string; img: string }[],
-    };
-    setGroups([...groups, newGroup]);
+    setDialogOpen(true);
   };
 
   const handleSelectGroup = (id: string) => {
@@ -64,6 +64,40 @@ export const GroupComponent: React.FC = () => {
     );
   };
 
+  const handleDialogOpen = () => {
+    setDialogOpen(true);
+  };
+
+  const handleDialogSubmit = () => {
+    setDialogOpen(false);
+    setSnackOpen(true);
+    const error = Math.random() > 0.5;
+    setSnackType(
+      error
+        ? { text: 'Fallo crear grupo', texture: 'Error' }
+        : { text: 'Grupo creado', texture: 'Success' }
+    );
+    if (!error) {
+      const newGroup = {
+        id: `${groups.length + 1}`,
+        text: `Grupo ${groups.length + 1}`,
+        members: [] as { id: string; name: string; img: string }[],
+      };
+      setGroups([...groups, newGroup]);
+    }
+  };
+
+  const handleSnackClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackOpen(false);
+  };
+
   return (
     <Box sx={{ display: 'flex', marginLeft: `${drawerWidth}px` }}>
       <GroupSidebar
@@ -77,8 +111,9 @@ export const GroupComponent: React.FC = () => {
           flexDirection: 'column',
           flexGrow: 1,
           justifyContent: 'space-between',
-          height: '95vh'
-        }}>
+          height: '95vh',
+        }}
+      >
         <Box
           sx={{
             display: 'flex',
@@ -101,8 +136,47 @@ export const GroupComponent: React.FC = () => {
               ))
             )}
         </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'left', padding: theme.spacing(2) }}>
-          <BlueButtonText text="Editar grupo" icon={<EditIcon/>} />
+
+        <CustomDialog
+          title="Crear grupo"
+          open={dialogOpen}
+          setOpen={setDialogOpen}
+          handleSubmit={handleDialogSubmit}
+          content={
+            <>
+              <InputText
+                direction="Row"
+                text="Nombre"
+                color={theme.palette.common.black}
+              />
+              <InputText
+                direction="Column"
+                text="Descripción"
+                color={theme.palette.common.black}
+              />
+              <InputText
+                direction="Row"
+                text="Miembros"
+                color={theme.palette.common.black}
+              />
+            </>
+          }
+          actionText="Crear"
+        />
+        <ToastBar
+          text={snacktype.text}
+          texture={snacktype.texture}
+          open={snackOpen}
+          onClose={handleSnackClose}
+        ></ToastBar>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'left',
+            padding: theme.spacing(2),
+          }}
+        >
+          <BlueButtonText text="Editar grupo" icon={<EditIcon />} />
         </Box>
       </Box>
     </Box>
